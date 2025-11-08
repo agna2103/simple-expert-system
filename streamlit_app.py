f7e6b0ef47d6d26503b9ebe62e12acaf6e11d069
agna2103 <161094297+agna2103@users.noreply.github.com>
2025-11-08 08:04:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,22 +9,13 @@ st.markdown(
         background-color: #2E003E;
         color: white;
     }
-    .banner-img {
-        position: fixed;
-        top: 15px;
-        right: 25px;
-        width: 100px;
-        border-radius: 8px;
-        box-shadow: 0px 0px 6px rgba(0,0,0,0.5);
-        z-index: 9999;
-    }
-    </style>
-
-    <img src="https://github.com/agna2103/simple-expert-system/blob/9aff16990de6ec130049b18a2e502c469492b66c/download.webp?raw=true" class="banner-img">
-    """,
     unsafe_allow_html=True
 )
-
+st.image(
+    "https://raw.githubusercontent.com/agna2103/simple-expert-system/9aff16990de6ec130049b18a2e502c469492b66c/download.webp",
+    width=120,
+    caption="Persiapan Emosional Pra-Nikah"
+)
 st.title("Persiapan Emosional Pra-Nikah")
 st.subheader("AI dalam Psikologi Kelompok 1")
 st.write("Selamat datang di aplikasi ini! 💜")
